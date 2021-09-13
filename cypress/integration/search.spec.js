43f71bacf6830f31248729eea45bb6de34645102
gorthyravi <gorthyravitheja@gmail.com>
2021-09-14 01:06:50 +0530

    Update search.spec.js

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 import '@percy/cypress'
 
-context('Visual Testing', () => {
+context('Visual Testing on Google Image search', () => {
   beforeEach(() => {
     cy.visit('https://www.google.com/imghp?hl=en')
   })
 
-  it('Results Screenshot 1', () => {
+  it('First Snapshot', () => {
     // https://on.cypress.io/type
     cy.get('.tsf').within(() => {
       cy.get('input:first')
@@ -23,7 +23,7 @@ context('Visual Testing', () => {
     })
   })
 
-  it('Results Screenshot 2', () => {
+  it('Second Snapshot', () => {
     // https://on.cypress.io/type
     cy.get('.tsf').within(() => {
       cy.get('input:first')
